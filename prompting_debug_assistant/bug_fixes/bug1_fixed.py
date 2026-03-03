375afeeca2ca3a039955604c2842615ce12e4904
Piro Vesho <pirovesho@gmail.com>
2026-03-03 14:33:44 +0100

    Add lines

--- prompting_debug_assistant/bug_fixes/bug1_fixed.py
+++ prompting_debug_assistant/bug_fixes/bug1_fixed.py
@@ -1,8 +1,10 @@
 def get_last_items(items, n):
     return items[-n:]
 
+
 fruits = ["apple", "banana", "cherry", "date", "elderberry"]
 print(get_last_items(fruits, 3))
+
 
 numbers = list(range(10))
 print(get_last_items(numbers, 10))
